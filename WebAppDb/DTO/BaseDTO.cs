6374c8c5de4932f28fc71e9d8d43f12b77bcb7c0
a8anassis <a8anassis@gmail.com>
2025-09-23 20:22:29 +0300

    feat: add base dto fro Student

---
+++ WebAppDb/DTO/BaseDTO.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebAppDb.DTO
+{
+    public abstract class BaseDTO
+    {
+        [Required(ErrorMessage = "The {0} is required.")]
+        public int Id { get; set; }
+
+        public BaseDTO()
+        {
+
+        }
+        public BaseDTO(int id)
+        {
+            Id = id;
+        }
+    }
+
+    //public abstract record BaseDTO(
+
+    //    [property: Required(ErrorMessage = "The {0} is required.")] 
+    //    int Id
+    //)
+    //{
+    //    public BaseDTO() : this(0)
+    //    {
+    //    }
+    //}
+}

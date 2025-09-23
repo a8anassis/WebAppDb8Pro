8bc05d93bc02770de4505d2729ff3921d60b29f0
a8anassis <a8anassis@gmail.com>
2025-09-23 20:54:12 +0300

    feat: add mapper class

---
+++ WebAppDb/Configuration/MapperConfig.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using WebAppDb.DTO;
+using WebAppDb.Models;
+
+namespace WebAppDb.Configuration
+{
+    public class MapperConfig : Profile
+    {
+        public MapperConfig()
+        {
+            CreateMap<StudentInsertDTO, Student>().ReverseMap();
+            CreateMap<StudentUpdateDTO, Student>().ReverseMap();
+            CreateMap<StudentReadOnlyDTO, Student>().ReverseMap();
+        }
+    }
+}

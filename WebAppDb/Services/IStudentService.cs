dca89a55dda05b96f5bbdbf4b636f22d99184260
a8anassis <a8anassis@gmail.com>
2025-09-24 19:34:14 +0300

    feat: add contract for student service

---
+++ WebAppDb/Services/IStudentService.cs
@@ -0,0 +1,13 @@
+﻿using WebAppDb.DTO;
+
+namespace WebAppDb.Services
+{
+    public interface IStudentService
+    {
+        StudentReadOnlyDTO? InsertStudent(StudentInsertDTO studentInsertDTO);
+        void UpdateStudent(StudentUpdateDTO studentUpdateDTO);
+        void DeleteStudent(int id);
+        StudentReadOnlyDTO? GetStudent(int id);
+        List<StudentReadOnlyDTO> GetAllStudents();
+    }
+}
